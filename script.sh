cc5c7fb06541ffbf1bb9669853507ae7ca106105
Anderson131 <aas20040831@gmail.com>
2024-05-22 06:47:25 -0400

    script.sh

--- script.sh
+++ script.sh
@@ -17,101 +17,97 @@ function list_users {
     awk -F: '{ print $1 ": " $6 }' /etc/passwd | sort
 }
 
-# Функция для вывода процессов
+# Функция для вывода процессов (добавлен пример с ошибкой)
 function list_processes {
     ps -e --sort pid
+    ls /nonexistent_directory  # Эта команда вызовет ошибку
 }
 
 # Инициализация переменных для логов и ошибок
 LOG_PATH=""
 ERROR_PATH=""
+ACTION=""
 
-# Обработка аргументов командной строки
-while getopts ":uphl:e:-:" opt; do
-    case $opt in
-        u)
+# Обработка аргументов командной строки с использованием getopt
+ARGS=$(getopt -o uphl:e: --long users,processes,help,log:,errors: -n "$0" -- "$@")
+if [ $? -ne 0 ]; then
+    show_help
+    exit 1
+fi
+
+eval set -- "$ARGS"
+
+# Разбор аргументов
+while true; do
+    case "$1" in
+        -u | --users)
             ACTION="users"
+            shift
             ;;
-        p)
+        -p | --processes)
             ACTION="processes"
+            shift
             ;;
-        h)
+        -h | --help)
             show_help
+            shift
             ;;
-        l)
-            LOG_PATH=$OPTARG
+        -l | --log)
+            LOG_PATH="$2"
+            shift 2
             ;;
-        e)
-            ERROR_PATH=$OPTARG
+        -e | --errors)
+            ERROR_PATH="$2"
+            shift 2
             ;;
-        -)
-            case "${OPTARG}" in
-                users)
-                    ACTION="users"
-                    ;;
-                processes)
-                    ACTION="processes"
-                    ;;
-                help)
-                    show_help
-                    ;;
-                log)
-                    LOG_PATH="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-                    ;;
-                errors)
-                    ERROR_PATH="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-                    ;;
-                *)
-                    echo "Неверный аргумент: --${OPTARG}" >&2
-                    show_help
-                    ;;
-            esac
+        --)
+            shift
+            break
             ;;
-        \?)
-            echo "Неверный опция: -$OPTARG" >&2
-            show_help
-            ;;
-        :)
-            echo "Опция -$OPTARG требует аргумент" >&2
+        *)
             show_help
             ;;
     esac
 done
 
 # Проверка доступности путей для логов и ошибок
-if [ -n "$LOG_PATH" ] && [ ! -w "$LOG_PATH" ]; then
-    echo "Невозможно записать в файл лога: $LOG_PATH" >&2
-    exit 1
+if [ -n "$LOG_PATH" ]; then
+    touch "$LOG_PATH" 2>/dev/null
+    if [ $? -ne 0 ]; then
+        echo "Невозможно записать в файл лога: $LOG_PATH" >&2
+        exit 1
+    fi
 fi
 
-if [ -n "$ERROR_PATH" ] && [ ! -w "$ERROR_PATH" ]; then
-    echo "Невозможно записать в файл ошибок: $ERROR_PATH" >&2
-    exit 1
+if [ -n "$ERROR_PATH" ]; then
+    touch "$ERROR_PATH" 2>/dev/null
+    if [ $? -ne 0 ]; then
+        echo "Невозможно записать в файл ошибок: $ERROR_PATH" >&2
+        exit 1
+    fi
 fi
 
 # Выполнение действия и вывод результатов
 case $ACTION in
     users)
-        OUTPUT=$(list_users 2>&1)
+        if [ -n "$LOG_PATH" ]; then
+            list_users > "$LOG_PATH"
+        else
+            list_users
+        fi
         ;;
     processes)
-        OUTPUT=$(list_processes 2>&1)
+        if [ -n "$LOG_PATH" ] && [ -n "$ERROR_PATH" ]; then
+            list_processes > "$LOG_PATH" 2> "$ERROR_PATH"
+        elif [ -n "$LOG_PATH" ]; then
+            list_processes > "$LOG_PATH" 2>&1
+        elif [ -n "$ERROR_PATH" ]; then
+            list_processes 2> "$ERROR_PATH"
+        else
+            list_processes
+        fi
         ;;
     *)
         show_help
         ;;
 esac
-
-# Запись вывода в файл или на экран
-if [ -n "$LOG_PATH" ]; then
-    echo "$OUTPUT" > "$LOG_PATH"
-else
-    echo "$OUTPUT"
-fi
-
-# Запись ошибок в файл или на экран
-if [ -n "$ERROR_PATH" ]; then
-    echo "$OUTPUT" > "$ERROR_PATH"
-else
-    echo "$OUTPUT" >&2
-fi
